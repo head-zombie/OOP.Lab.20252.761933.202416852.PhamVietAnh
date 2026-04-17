b4b9223584addd2540ff32e96ba3cb6c898f7a78
head.zombie <pvanh780@gmail.com>
2026-04-17 22:12:45 +0700

    exercise 8

---
+++ Lab03/OtherProjects/src/soict/dsai/Lab01/FirstDialog.java
@@ -0,0 +1,9 @@
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world ! how are you ?");
+        System.exit(0);
+    }
+}
